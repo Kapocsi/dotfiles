115da12614b351baa591b2192d0980b8ab6cced9
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-03-22 10:38:30 -0600

    add telescope ui for code actions
    setup for code actions

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -39,10 +39,27 @@ return {
         cmd = "Neotree"
     },
     {
+        "/nvim-telescope/telescope-ui-select.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim"
+        }
+    },
+    {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         dependencies = {
             'nvim-lua/plenary.nvim'
-        }
+        },
+        config = function()
+            -- This is your opts table
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
     }
 }
