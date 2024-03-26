c82250a08f528b971be41fad23f9911911394e68
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-03-26 11:25:42 -0600

    move to template file for ssh config

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-TMUX=$(which tmux)
-envsubst < ./.config/alacritty/alacritty.toml.template > ./.config/alacritty/alacritty.toml
+TMUX=$(which tmux) envsubst < ./.config/alacritty/alacritty.toml.template > ./.config/alacritty/alacritty.toml
+
+KEYCHAIN_LINE=$(if [[ "$OSTYPE" == "darwin"* ]]; then echo "UseKeychain yes"; else echo "# Removed use keychain for non-mac "; fi)\
+    envsubst < ./.ssh/config.tmpl > ./.ssh/config
 
