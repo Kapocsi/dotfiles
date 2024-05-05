930bcf311ff2703e7b1cf49f8bb0b56a35e726fb
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-05-05 14:48:41 -0600

    changes

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -8,7 +8,7 @@ require'nvim-treesitter.configs'.setup {
     auto_install = true,
 
     highlight = {
-        enable = true,
+        enable = false,
         additional_vim_regex_highlighting = false
     },
 
--- packages/wait4exit/wait4exit.c
+++ packages/wait4exit/wait4exit.c
@@ -6,56 +6,34 @@
 #include <sys/time.h>
 
 int main(int argc, char **argv) {
-    pid_t ppid, fpid;
-    struct kevent kev;
-    int kq;
-    int kret;
-    struct timespec timeout;
-
-    if ( argc > 2 ) {
+    int ppid = getppid();
+    int fpid;
+    
+    if ( argc != 2 ) {
         fprintf(stderr, "Please quote the command you want to run\n");
         exit(-1);
     }
-
-    ppid = getppid();
-
     fpid = fork();
     if ( fpid == -1 ) {
         perror("fork");
         exit(-1);
     }
 
+    // Exit the initial fork
     if ( fpid != 0 ) {
         exit(0);
     }
 
-    EV_SET(&kev, ppid, EVFILT_PROC, EV_ADD, NOTE_EXIT, 0, 0);
-
-    kq = kqueue();
-    if ( kq == -1 ) {
-        perror("kqueue");
-        exit(-1);
+    int timeout = 3600 * 60 * 8;
+    while ((ppid = getppid()) != 1 && timeout > 1) {
+        timeout--;
+        sleep(1);
     }
 
-    kret = kevent(kq, &kev, 1, NULL, 0, NULL);
-    if ( kret == -1 ) {
-        perror("kevent");
-        exit(-1);
+    if (timeout == 0 ) {
+        exit(0);
     }
 
-    timeout.tv_sec = ( 8 /*hours*/ * 60 /*minutes per hour*/ * 60 /*seconds per minute*/ );
-    timeout.tv_nsec = 0;
+    system(argv[1]);
 
-    kret = kevent(kq, NULL, 0, &kev, 1, &timeout);
-    if ( kret == -1 ) {
-        perror("kevent");
-        exit(-1);
-    }
-
-    if ( kret > 0 ) {
-        system(argv[1]);
-    }
-    /* ( kret == 0 ) means timeout; don't do anything */
-
-    exit(0);
 }
