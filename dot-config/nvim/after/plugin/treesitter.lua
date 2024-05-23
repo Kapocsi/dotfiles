10279527081c102a6d181f1459b64fc8575e920b
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-05-23 11:27:28 -0600

    fixes to neovim config

--- dot-config/nvim/after/plugin/treesitter.lua
+++ dot-config/nvim/after/plugin/treesitter.lua
@@ -1,32 +1,31 @@
 vim.filetype.add({
     pattern = {
-        ['.*%.tmpl$'] = function(path, bufnr)
-            local base_name = path:match('(.+)%.tmpl$')
+        ['.*tmpl'] = function(path)
+            local base_name = path:match('.*/(.+)%.tmpl$')
+            vim.print(base_name)
             if base_name then
                 local filetype = vim.filetype.match({
                     filename = base_name
                 })
+
+                vim.print(filetype)
                 return filetype
             end
         end
     }
 })
 
+---@diagnostic disable-next-line: missing-parameter
+require'nvim-ts-autotag'.setup()
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {},
-    modules = {},
-    ignore_install = {},
-
+    ensure_installed = "all",
     sync_install = false,
-
     auto_install = true,
+    ignore_install = {},
 
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
     },
-
-    autotag = {
-        enable = true
-    }
+    modules = {}
 }
