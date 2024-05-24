2762dcfd6c2417e1d76044b6f8a7f2ba2221c7b7
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-05-24 16:30:00 -0600

    change hostname color

--- ZSH/prompt.zsh
+++ ZSH/prompt.zsh
@@ -6,7 +6,7 @@ zi light romkatv/gitstatus
 
 setopt prompt_subst
 b='%F{blue}'
-hostname='%F{39}$([ ! -z $SSH_TTY ] && printf "󰴽 $HOST" ) %f'
+hostname='%F{196}$([ ! -z $SSH_TTY ] && printf "󰴽 $HOST" ) %f'
 git_status='$([ ! -z "${GITSTATUS_PROMPT}" ] && echo "(${GITSTATUS_PROMPT}%39F)%f" )'
 current_dir='%1~'
 starting_arrow='%F{%(?.046.196)}󰄾%f'
