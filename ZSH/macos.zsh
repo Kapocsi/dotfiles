98a04904bd9edbd886f89b722281487594346c2a
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-07-15 10:03:35 -0600

    changes

--- ZSH/macos.zsh
+++ ZSH/macos.zsh
@@ -6,6 +6,5 @@ fi
 export MANPATH="/opt/local/share/man:$MANPATH"
 
 alias "chrome"="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias python="python3.12"
 alias code="open -a Visual Studio Code"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
