7c8815bc610088db05f89fdda890c4ee0cd399c5
Thomas Kapocsi <thomas@kapocsi.ca>
2024-08-01 16:38:57 -0600

    Add browser var and try to fix steam, gpg?

--- ZSH/linux.zsh
+++ ZSH/linux.zsh
@@ -7,10 +7,10 @@ alias pbpaste='xclip -selection clipboard -o'
 
 # If we are not in a firejail 
 if firejail --list 2> /dev/null > /dev/null; then;
-
-eval $(keychain --agents ssh --eval --quiet)
-
+    eval $(keychain --agents ssh --eval --quiet)
 fi
+
+export BROWSER=$(which brave)
 
 
 alias open="xdg-open"
