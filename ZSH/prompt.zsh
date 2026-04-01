0cecf6e3c65d2f95062bb21a0a0483460ea13925
Thomas Kapocsi <thomas@kapocsi.ca>
2026-04-01 07:39:21 -0600

    Switch to starship

--- ZSH/prompt.zsh
+++ ZSH/prompt.zsh
@@ -1,16 +1,3 @@
-## Prompt ###  
-zi ice src="gitstatus.prompt.zsh"
-zi light romkatv/gitstatus
-
-
-
-setopt prompt_subst
-b='%F{blue}'
-if [[ "$OSTYPE" != "darwin"* ]] then 
-fire='$(firejail --list > /dev/null 2>/dev/null || echo "󰒄")'
-fi
-hostname='%F{196}$([ ! -z $SSH_TTY ] && printf "󰴽 $HOST" ) %f'
-git_status='$([ ! -z "${GITSTATUS_PROMPT}" ] && echo "(${GITSTATUS_PROMPT}%39F)%f" )'
-current_dir='%1~'
-starting_arrow='%F{%(?.046.196)}󰄾%f'
-export PROMPT="$fire$hostname%F{39}$current_dir $git_status$starting_arrow %f" 
+### Prompt ###
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+eval "$(starship init zsh)"
